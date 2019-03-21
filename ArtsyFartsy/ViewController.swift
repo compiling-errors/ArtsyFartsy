8affb731c535120a1cf59e3eb114e10bb387a53d
MICHAEL BENTON <5005575921@wce111-17.local>
2019-03-21 16:51:44 -0700

    Initial Commit

---
+++ ArtsyFartsy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ArtsyFartsy
+//
+//  Created by MICHAEL BENTON on 3/21/19.
+//  Copyright © 2019 MICHAEL BENTON. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
