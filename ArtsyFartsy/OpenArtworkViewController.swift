b640688288d8100493aff3b7d1cf5636299e0f97
mllillybean <you@example.com>
2019-04-24 03:33:33 -0700

    Update Like Button
    Like button allows user to like artwork

--- ArtsyFartsy/OpenArtworkViewController.swift
+++ ArtsyFartsy/OpenArtworkViewController.swift
@@ -15,11 +15,14 @@ class OpenArtworkViewController: UIViewController {
     //Create variable to save individual user artwork
     var artwork: PFObject?
     
+    let artworkPost = PFObject(className: "ArtworkPosts")
+    
     @IBOutlet weak var artworkImgView: UIImageView!
     @IBOutlet weak var profileImgView: UIImageView!
     @IBOutlet weak var usernameLabel: UILabel!
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var moreinfoLabel: UILabel!
+    @IBOutlet weak var likesLabel: UILabel!
     
     
     override func viewDidLoad() {
@@ -43,20 +46,122 @@ class OpenArtworkViewController: UIViewController {
         
         artworkImgView.af_setImage(withURL: url)
         
+        //Load in likes
+        loadLikeCounter()
+        
     }
+    
+    func loadLikeCounter(){
+        
+        let query = PFQuery(className:"ArtworkPosts")
+    
+        query.getObjectInBackground(withId: (artwork?.objectId)!) { (tempCount: PFObject?, error: Error?) in
+            if let error = error {
+                //The query returned an error
+                print(error.localizedDescription)
+            } else {
+                //The object has been retrieved
+                let stringstuff = tempCount!.object(forKey: "name")
+//                self.likesLabel.text = stringstuff as! String
+                
+                
+                let x : Int = tempCount!.object(forKey: "likeCount") as! Int
+                let xNSNumber = x as NSNumber
+                let xString : String = xNSNumber.stringValue
+                self.likesLabel.text = xString
+                
+                //                let newLikeCount = self.artworkPost["likeCount"]
+//                let theCount = (newLikeCount as AnyObject).stringValue
+//
+//                self.likesLabel.text = theCount
+                
+                
+            }
+        }
+}
+
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         
         //From parse documentation
-        let query = PFQuery(className: "ArtworkPosts")
         
         
         
     }
     
     @IBAction func likeButton(_ sender: Any) {
+        // Get the objectId of the current artwork
+        let imageId = artwork?.objectId
+        let currentUser = PFUser.current()?.username
+        
+        print("imageId = ", imageId!)
+        
+        //Add username into ArtworkPosts table, to "add a like"
+                //Check if user has already clicked like
+        
+        
+        let query = PFQuery(className:"ArtworkPosts")
+        
+        query.whereKey("objectId", equalTo:artwork?.objectId!)
+        query.whereKey("usersClickedLike", equalTo:currentUser!)
+        
+        query.getFirstObjectInBackground { (object: PFObject?, error: Error?) in
+            if let error = error {
+                // The query succeeded but no matching result was found
+                let query = PFQuery(className:"ArtworkPosts")
+                query.getObjectInBackground(withId: imageId!) { (userClickedLike: PFObject?, error: Error?) in
+                    if let error = error {
+                        print("Error, like was not saved.", error.localizedDescription)
+                    } else if let userClickedLike = userClickedLike {
+                        userClickedLike.addUniqueObjects(from: [PFUser.current()?.username], forKey:"usersClickedLike")
+                        
+                        userClickedLike.saveInBackground()
+                        userClickedLike.incrementKey("likeCount")
+                        userClickedLike.saveInBackground()                        //Save the new like to the likeCount column
+                        print("Like saved.")
+                        
+                        self.loadLikeCounter()
+                        
+                        //Calculate and display likes on open artwork screen
+                    }
+                }
+                
+            }
+                
+            
+        }
+        
+//        let query = PFQuery(className:"ArtworkPosts")
+//        let theUser = query.whereKey("usersClickedLike", equalTo: currentUser!)
+//
+//        if false == false  {
+//
+//               //do nothing
+//        } else {
+//            let query = PFQuery(className:"ArtworkPosts")
+//            query.getObjectInBackground(withId: imageId) { (userClickedLike: PFObject?, error: Error?) in
+//                if let error = error {
+//                    print("Error, objectId could not be found. Like was not saved.", error.localizedDescription)
+//                } else if let userClickedLike = userClickedLike {
+//                    userClickedLike.addUniqueObjects(from: [PFUser.current()?.username], forKey:"usersClickedLike")
+//                    userClickedLike.saveInBackground()
+//
+//                    print("Like saved.")
+//
+//                    //Calculate and display likes on open artwork screen
+//                }
+//            }
+//        }
+        
     }
+        
+        
+        //Display count for likes
+        
+        //Get current like count, using saved usernames for selected artwork
+        
+        //likesLabel.Text = query.countObjects
     
     
     @IBAction func followButton(_ sender: Any) {
